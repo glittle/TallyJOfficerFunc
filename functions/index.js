3bf08a269f7fb960727350e01ac0c020cdec66a3
Glen Little <glen.little@gmail.com>
2019-03-04 08:37:54 -0700

    working better

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 // back end processes for TallyJ Officer
 // include a version number in logs to know when a new version of this code is in use
-const version = 44;
+const version = 54;
 
 console.log('version', version, 'registered');
 
@@ -40,10 +40,10 @@ exports.onVotingChange = functions.database
                 };
                 checkIfCompleted(round, newValue.members);
 
+                // make a round
                 var timestamp = new Date().getTime();
                 var id = `${newValue.positionId}_${timestamp}`;
                 round.id = id;
-                // make a round
                 var path = `/votingRounds/${electionKey}/${id}`;
                 console.log('add round', path);
                 db.ref(path).set(round);
@@ -53,6 +53,12 @@ exports.onVotingChange = functions.database
                 console.log('close voting for this round', path);
                 db.ref(path).update({
                     votingOpen: false
+                });
+
+                path = `/positions/${electionKey}/${newValue.positionId}`;
+                console.log('update position', path);
+                db.ref(path).update({
+                    electedId: round.electedId
                 });
             }
         } else {
@@ -67,16 +73,18 @@ function checkIfCompleted(round, memberIds) {
     var numVotesRequired = 1 + Math.floor(memberIds.length / 2);
 
     var membersWithEnoughVotes = memberIds.filter(
-        m => votes.filter(v => v.voteId === m.id).length >= numVotesRequired
+        memberId => votes.filter(v => v.voteId === memberId).length >= numVotesRequired
     );
+
+    console.log('check', numVotesRequired, memberIds, votes, membersWithEnoughVotes);
+
     if (membersWithEnoughVotes.length) {
-        // check if multiple? - can't happen...
-        var elected = membersWithEnoughVotes[0];
-        round.electedId = elected.voteId;
         round.completed = true;
+        // check if multiple? - can't happen...
+        round.electedId = membersWithEnoughVotes[0];
     } else {
-        round.electedId = null;
         round.completed = false;
+        round.electedId = null;
     }
 }
 
@@ -124,12 +132,29 @@ exports.onElectionChange = functions.database
 
                     // reset their voting flags
                     db.ref(`members/${electionKey}/${memberInfo.id}`).update({
-                        voting: false,
+                        voting: true,
+                        preferNot: false,
                         voted: false
                     });
                 })
 
                 return 'distributed symbols';
+            });
+
+        } else if (!newValue.votingOpen && oldValue.votingOpen) {
+            console.log('voting just closed');
+
+            db.ref(`/members/${electionKey}`).once('value', snapshot => {
+                var memberList = snapshot.val();
+
+                Object.keys(memberList).forEach((memberId, i) => {
+                    // reset their voting flags
+                    db.ref(`members/${electionKey}/${memberId}`).update({
+                        voting: false
+                    });
+                })
+
+                return 'reset voting status for every member';
             });
 
         } else {
@@ -154,7 +179,7 @@ exports.onUserStatusChanged = functions.database.ref('/users/{uid}').onUpdate(
         if (status === 'offline') {
             // only concerned about noticing when someone leaves the election
 
-            var path = `/elections/${electionKey}/members/${memberId}`;
+            var path = `/members/${electionKey}/${memberId}`;
             console.log('set', path);
 
             db.ref(path).update({
