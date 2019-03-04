e115466a153cc8d0993f69a63dbe68c37acc387d
Glen Little <glen.little@gmail.com>
2019-03-03 23:16:30 -0700

    voting mostly working

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 // back end processes for TallyJ Officer
 // include a version number in logs to know when a new version of this code is in use
-const version = 31;
+const version = 44;
 
 console.log('version', version, 'registered');
 
@@ -10,117 +10,127 @@ admin.initializeApp();
 // const firestore = admin.firestore();
 const db = admin.database();
 
-exports.onElectionChange = functions.database.ref('/elections/{electionKey}')
-    .onUpdate((change, context) => {
+
+exports.onVotingChange = functions.database
+    .ref('/voting/{electionKey}').onUpdate((change, context) => {
         const electionKey = context.params.electionKey;
-        console.log('election changed', electionKey);
+        console.log('voting changed', electionKey);
+
         const newValue = change.after.val();
         const oldValue = change.before.val();
-        console.log('new', newValue, 'old', oldValue);
 
-        if (newValue.votingOpen && !oldValue.votingOpen) {
-            console.log('voting just opened', newValue);
-            // need to assign voting slots to members
+        // console.log('new', newValue, 'old', oldValue);
 
-            // randomize the order
-            var memberIds = newValue.members.map(id => {
-                return { id: id, sort: Math.random() };
-            });
-            memberIds.sort((a, b) => (a.sort < b.sort ? -1 : 1));
+        if (JSON.stringify(newValue.votes) !== JSON.stringify(oldValue.votes)) {
 
-            var voteSymbols = Object.keys(newValue.currentVotes);
-
-            if (voteSymbols.length !== memberIds.length) {
-                // something went wrong!
-                console.log('voteSymbol !== membercount', voteSymbols.length, memberIds.length);
-                return false;
-            }
-
-            memberIds.forEach((memberInfo, i) => {
-                // tell this member their symbol
-                var symbol = voteSymbols[i];
-                var path = `/users/${electionKey}/${memberInfo.id}`;
-                console.log('set', electionKey, memberInfo.id, symbol, path);
-
-                db.ref(path).update({ symbol: symbol });
-
-                //--> Firestore is way too slow to distribute info to members - sometimes up to 30 seconds!
-
-                // firestore.collection('elections').doc(electionKey).collection('memberSymbols').doc(memberInfo.id)
-                //     .set({
-                //         symbol: voteSymbols[i]
-                //     })
-                //     .then(() => {
-                //         console.log("Symbol successfully written!");
-                //         return true;
-                //     })
-                //     .catch((error) => {
-                //         console.error("Error writing symbol: ", error);
-                //     });
-
-
-            })
-        } else if (JSON.stringify(newValue.currentVotes) !== JSON.stringify(oldValue.currentVotes)) {
             console.log('someone just voted');
-            var votesDict = newValue.currentVotes;
-            var votesList = Object.keys(votesDict).map(k => { return { symbol: k, vote: votesDict[k] }; });
-            var numVoted = votesList.filter(v => v.vote).length;
-            var numNotVoted = votesList.filter(v => !v.vote).length;
+            var votesDict = newValue.votes;
+            var votesList = Object.keys(votesDict).map(k => { return { symbol: k, voteId: votesDict[k] }; });
+            var numVoted = votesList.filter(v => v.voteId).length;
+            var numNotVoted = votesList.filter(v => !v.voteId).length;
 
             var numMembers = newValue.members.length;
             console.log('members:', numMembers, 'voted:', numVoted, 'notVoted:', numNotVoted);
 
             if (numVoted === numMembers) {
-                // process this vote!
-
-                const timeStamp = new Date().getTime();
+                // process this vote into a result!
 
                 var round = {
-                    id: newValue.positionIdToVoteFor +
-                        "_" +
-                        timeStamp,
-                    // want to number it based on how many are already there, but can't easily with firestore
-                    // ("00" + this.positionRounds.length).slice(-3),
-                    votes: votes
+                    votes: votesList
                 };
-                this.checkIfCompleted(newValue.members, round);
+                checkIfCompleted(round, newValue.members);
 
-                var path = `/elections/${electionKey}/positions/${newValue.positionIdToVoteFor}/rounds/${round.id}`;
-                console.log('set', path);
-
+                var timestamp = new Date().getTime();
+                var id = `${newValue.positionId}_${timestamp}`;
+                round.id = id;
+                // make a round
+                var path = `/votingRounds/${electionKey}/${id}`;
+                console.log('add round', path);
                 db.ref(path).set(round);
 
-                // firestore
-                //     .collection("elections")
-                //     .doc(electionKey)
-                //     .collection("positions")
-                //     .doc(newValue.positionIdToVoteFor)
-                //     .collection("rounds")
-                //     .doc(round.id)
-                //     .set(round)
-                //     .then(() => console.log('saved round', timeStamp))
-                //     .catch(error => {
-                //         console.log("Error saving round.", error);
-                //     });
-
+                // close voting for this round
                 path = `/elections/${electionKey}`;
-                console.log('set', path);
-
+                console.log('close voting for this round', path);
                 db.ref(path).update({
                     votingOpen: false
                 });
-
-                // firestore
-                //     .collection("elections")
-                //     .doc(electionKey)
-                //     .update({
-                //         votingOpen: false
-                //     })
-                //     .then(() => console.log('voting closed'))
-                //     .catch(error => {
-                //         console.log("Error closing voting.", error);
-                //     });
             }
+        } else {
+            console.log('new', newValue, 'old', oldValue);
+        }
+        return 'done';
+    });
+
+
+function checkIfCompleted(round, memberIds) {
+    var votes = round.votes;
+    var numVotesRequired = 1 + Math.floor(memberIds.length / 2);
+
+    var membersWithEnoughVotes = memberIds.filter(
+        m => votes.filter(v => v.voteId === m.id).length >= numVotesRequired
+    );
+    if (membersWithEnoughVotes.length) {
+        // check if multiple? - can't happen...
+        var elected = membersWithEnoughVotes[0];
+        round.electedId = elected.voteId;
+        round.completed = true;
+    } else {
+        round.electedId = null;
+        round.completed = false;
+    }
+}
+
+
+exports.onElectionChange = functions.database
+    .ref('/elections/{electionKey}').onUpdate((change, context) => {
+        const electionKey = context.params.electionKey;
+        console.log('election changed', electionKey);
+
+        const newValue = change.after.val();
+        const oldValue = change.before.val();
+
+        // console.log('new', newValue, 'old', oldValue);
+
+        if (newValue.votingOpen && !oldValue.votingOpen) {
+            console.log('voting just opened', newValue);
+            // need to assign voting slots to members
+
+            db.ref(`/voting/${electionKey}`).once('value', snapshot => {
+                var voting = snapshot.val();
+
+                // randomize the order
+                var voteSymbols = Object.keys(voting.votes);
+
+                var memberIds = voting.members.map(id => {
+                    return { id: id, sort: Math.random() };
+                });
+                memberIds.sort((a, b) => (a.sort < b.sort ? -1 : 1));
+
+                if (voteSymbols.length !== memberIds.length) {
+                    // something went wrong!
+                    console.log('voteSymbol !== membercount', voteSymbols.length, memberIds.length);
+                    return false;
+                }
+
+                var path = `/voterSymbols/${electionKey}`;
+                db.ref(path).remove();
+
+                memberIds.forEach((memberInfo, i) => {
+                    // tell this member their symbol
+                    var symbol = voteSymbols[i];
+                    path = `/voterSymbols/${electionKey}/${memberInfo.id}`;
+                    console.log('give symbol', electionKey, memberInfo.id, symbol, path);
+                    db.ref(path).update({ symbol: symbol });
+
+                    // reset their voting flags
+                    db.ref(`members/${electionKey}/${memberInfo.id}`).update({
+                        voting: false,
+                        voted: false
+                    });
+                })
+
+                return 'distributed symbols';
+            });
 
         } else {
             console.log('new', newValue, 'old', oldValue);
@@ -128,23 +138,6 @@ exports.onElectionChange = functions.database.ref('/elections/{electionKey}')
         return 'done';
     });
 
-function checkIfCompleted(memberIds, round) {
-    var votes = round.votes;
-    var numVotesRequired = 1 + Math.floor(memberIds.length / 2);
-
-    var membersWithEnoughVotes = membersIds.filter(
-        m => votes.filter(v => v.id === m.id).length >= numVotesRequired
-    );
-    if (membersWithEnoughVotes.length) {
-        // check if multiple? - can't happen...
-        var elected = membersWithEnoughVotes[0];
-        round.elected = elected;
-        round.completed = true;
-    } else {
-        round.elected = false;
-        round.completed = false;
-    }
-}
 
 exports.onUserStatusChanged = functions.database.ref('/users/{uid}').onUpdate(
     (change, context) => {
