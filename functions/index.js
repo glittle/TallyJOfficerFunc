32bc01d50d61f32bfb91343a37b4392489cd18ee
Glen Little <glen.little@gmail.com>
2019-03-06 08:52:12 -0700

    various improvements

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,6 @@
 // back end processes for TallyJ Officer
 // include a version number in logs to know when a new version of this code is in use
-const version = 54;
+const version = 59;
 
 console.log('version', version, 'registered');
 
@@ -23,42 +23,45 @@ exports.onVotingChange = functions.database
 
         if (JSON.stringify(newValue.votes) !== JSON.stringify(oldValue.votes)) {
 
-            console.log('someone just voted');
+            console.log('votes changed');
             var votesDict = newValue.votes;
             var votesList = Object.keys(votesDict).map(k => { return { symbol: k, voteId: votesDict[k] }; });
             var numVoted = votesList.filter(v => v.voteId).length;
             var numNotVoted = votesList.filter(v => !v.voteId).length;
 
-            var numMembers = newValue.members.length;
-            console.log('members:', numMembers, 'voted:', numVoted, 'notVoted:', numNotVoted);
+            console.log('voted:', numVoted, ' - notVoted:', numNotVoted);
 
-            if (numVoted === numMembers) {
+            if (numNotVoted === 0) {
                 // process this vote into a result!
+                db.ref(`/members/${electionKey}`).once('value', snapshot => {
+                    var members = snapshot.val();
+                    var memberIds = Object.keys(members);
 
-                var round = {
-                    votes: votesList
-                };
-                checkIfCompleted(round, newValue.members);
+                    var round = {
+                        votes: votesList
+                    };
+                    checkIfCompleted(round, memberIds);
 
-                // make a round
-                var timestamp = new Date().getTime();
-                var id = `${newValue.positionId}_${timestamp}`;
-                round.id = id;
-                var path = `/votingRounds/${electionKey}/${id}`;
-                console.log('add round', path);
-                db.ref(path).set(round);
+                    // make a round
+                    var timestamp = new Date().getTime();
+                    var id = `${newValue.positionId}_${timestamp}`;
+                    round.id = id;
+                    var path = `/votingRounds/${electionKey}/${id}`;
+                    console.log('add round', path);
+                    db.ref(path).set(round);
 
-                // close voting for this round
-                path = `/elections/${electionKey}`;
-                console.log('close voting for this round', path);
-                db.ref(path).update({
-                    votingOpen: false
-                });
+                    // close voting for this round
+                    path = `/elections/${electionKey}`;
+                    console.log('close voting for this round', path);
+                    db.ref(path).update({
+                        votingOpen: false
+                    });
 
-                path = `/positions/${electionKey}/${newValue.positionId}`;
-                console.log('update position', path);
-                db.ref(path).update({
-                    electedId: round.electedId
+                    path = `/positions/${electionKey}/${newValue.positionId}`;
+                    console.log('update position', path);
+                    db.ref(path).update({
+                        electedId: round.electedId
+                    });
                 });
             }
         } else {
@@ -106,36 +109,45 @@ exports.onElectionChange = functions.database
             db.ref(`/voting/${electionKey}`).once('value', snapshot => {
                 var voting = snapshot.val();
 
-                // randomize the order
-                var voteSymbols = Object.keys(voting.votes);
+                db.ref(`/members/${electionKey}`).once('value', snapshot => {
+                    var members = snapshot.val();
+                    var membersList = Object.values(members);
+                    var participantsList = membersList.filter(m => m.participating);
 
-                var memberIds = voting.members.map(id => {
-                    return { id: id, sort: Math.random() };
-                });
-                memberIds.sort((a, b) => (a.sort < b.sort ? -1 : 1));
-
-                if (voteSymbols.length !== memberIds.length) {
-                    // something went wrong!
-                    console.log('voteSymbol !== membercount', voteSymbols.length, memberIds.length);
-                    return false;
-                }
-
-                var path = `/voterSymbols/${electionKey}`;
-                db.ref(path).remove();
-
-                memberIds.forEach((memberInfo, i) => {
-                    // tell this member their symbol
-                    var symbol = voteSymbols[i];
-                    path = `/voterSymbols/${electionKey}/${memberInfo.id}`;
-                    console.log('give symbol', electionKey, memberInfo.id, symbol, path);
-                    db.ref(path).update({ symbol: symbol });
-
-                    // reset their voting flags
-                    db.ref(`members/${electionKey}/${memberInfo.id}`).update({
-                        voting: true,
-                        preferNot: false,
-                        voted: false
+                    var participantInfos = participantsList.map(participant => {
+                        return {
+                            id: participant.id,
+                            sort: Math.random()
+                        };
                     });
+
+                    var voteSlots = Object.keys(voting.votes);
+
+                    if (voteSlots.length !== participantInfos.length) {
+                        // something went wrong!
+                        console.log('num voting !== participants', voteSlots.length, participantInfos.length);
+                        return false;
+                    }
+
+                    var path = `/voterSymbols/${electionKey}`;
+                    db.ref(path).remove();
+
+                    // randomize the order
+                    participantInfos.sort((a, b) => (a.sort < b.sort ? -1 : 1));
+
+                    participantInfos.forEach((participantInfo, i) => {
+                        // tell this member their symbol
+                        var symbol = voteSlots[i];
+                        path = `/voterSymbols/${electionKey}/${participantInfo.id}`;
+                        console.log('give symbol', electionKey, participantInfo.id, symbol, path);
+                        db.ref(path).update({ symbol: symbol });
+
+                        // reset their voting flags
+                        db.ref(`members/${electionKey}/${participantInfo.id}`).update({
+                            preferNot: false,
+                            voted: false
+                        });
+                    })
                 })
 
                 return 'distributed symbols';
@@ -145,20 +157,25 @@ exports.onElectionChange = functions.database
             console.log('voting just closed');
 
             db.ref(`/members/${electionKey}`).once('value', snapshot => {
-                var memberList = snapshot.val();
+                var members = snapshot.val();
+                var membersList = Object.values(members);
 
-                Object.keys(memberList).forEach((memberId, i) => {
+                membersList.forEach(member => {
                     // reset their voting flags
-                    db.ref(`members/${electionKey}/${memberId}`).update({
+                    // if (member.connected) {
+                    var path = `/members/${electionKey}/${member.id}`;
+                    console.log(`set voting false for ${path}`)
+                    db.ref(path).update({
                         voting: false
                     });
+                    // }
                 })
 
                 return 'reset voting status for every member';
             });
 
         } else {
-            console.log('new', newValue, 'old', oldValue);
+            console.log('misc election change new', newValue, 'old', oldValue);
         }
         return 'done';
     });
@@ -180,7 +197,7 @@ exports.onUserStatusChanged = functions.database.ref('/users/{uid}').onUpdate(
             // only concerned about noticing when someone leaves the election
 
             var path = `/members/${electionKey}/${memberId}`;
-            console.log('set', path);
+            console.log('connected false for', path);
 
             db.ref(path).update({
                 connected: false
